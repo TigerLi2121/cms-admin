b6039bee3670672f5fd352352f7100d4fae94afb
tigerli <83432087@qq.com>
2023-07-19 10:58:14 +0800

    init

---
+++ src/main/java/com/mm/common/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.mm.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置
+ *
+ * @author lwl
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis拦截
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+}
